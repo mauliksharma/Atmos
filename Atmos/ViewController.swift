30900b63ca2e64c5905010a4657018521d2f80f3
Maulik Sharma <mauliksharma@Mauliks-MacBook.local>
2018-11-22 08:51:46 +0530

    Added subViews

---
+++ Atmos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Atmos
+//
+//  Created by Maulik Sharma on 22/11/18.
+//  Copyright © 2018 Geekskool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
